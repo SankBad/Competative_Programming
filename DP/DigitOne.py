a09d1ec6b47f2e696e961ba4b0d1740322143aaa
Sanbad <29823086+SankBad@users.noreply.github.com>
2019-08-01 01:38:56 +0530

    Create DigitOne.py

---
+++ DP/DigitOne.py
@@ -0,0 +1,12 @@
+def DigitOne(n):
+    while(int(n)>9):
+        string = str(n)
+        n = 0
+        for i in string:
+            n += int(i)
+    return n
+
+t=int(input())
+for i in range(t):
+    a=int(input())
+    print(DigitOne(a))
