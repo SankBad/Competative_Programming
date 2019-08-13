1c8d0ad61c5bdb9aee83aaa5b4cfccf697054972
Sanbad <29823086+SankBad@users.noreply.github.com>
2019-08-13 18:46:11 +0530

    Create Pascal.py

---
+++ Array/Pascal.py
@@ -0,0 +1,13 @@
+def solve(A):
+        SolArr = [[0 for x in range(A+1)] for y in range(A+1)]
+        SolArr[1][1]=1
+        for i in range(2, A+1):
+            for j in range(1, i+1):
+                SolArr[i][j] = SolArr[i-1][j] + SolArr[i-1][j-1]
+        
+        SolArr1 = [[0 for x in range(y+1)] for y in range(A)]
+        for i in range(A):
+            for j in range(i+1):
+                SolArr1[i][j] = SolArr[i+1][j+1]
+                
+        return SolArr1
