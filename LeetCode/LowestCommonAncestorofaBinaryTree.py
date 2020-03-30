c46293e3c4dcccadfd02f522be2bcbfd249bdfd4
Sanbad <29823086+SankBad@users.noreply.github.com>
2020-03-29 20:04:48 -0400

    Create LowestCommonAncestorofaBinaryTree.py

---
+++ LeetCode/LowestCommonAncestorofaBinaryTree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def SearchInt(self, root: 'TreeNode', p: 'TreeNode'):
+        if root==None:
+            return False
+        if root.val == p:
+            return True
+        lefexist = self.SearchInt(root.left, p)
+        if lefexist:
+            return True
+        rightexist = self.SearchInt(root.right, p)
+        return rightexist
+        
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        First = ((self.SearchInt(root.left, p.val))&(self.SearchInt(root.left, q.val)))
+        Second = ((self.SearchInt(root.right, p.val))&(self.SearchInt(root.right, q.val)))
+        if ((First==False) & (Second==False)):
+            return root
+        elif (First==True):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (Second==True):
+            return self.lowestCommonAncestor(root.right, p, q)
+            
+            
+            
+        
+        
