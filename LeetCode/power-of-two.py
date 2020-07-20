e49da2887d5eec24fbf8ac182f023813ac9b8336
Sanbad <29823086+SankBad@users.noreply.github.com>
2020-07-19 23:57:23 -0400

    Update power-of-two.py

--- LeetCode/power-of-two.py
+++ LeetCode/power-of-two.py
@@ -4,14 +4,11 @@ class Solution:
         if n>0:
             while(n!=1 and b ==0):
                 n, b = divmod(n,2)
-        if n<=-1:
+        if n<=0:
             return False
-        if n<0:
-            n = -1/n
-            while(n!=1 and b ==0):
-                n, b = divmod(n,2)
         
-        if n==1:
+        
+        if (n==1 and b==0):
             return True
         else:
             return False
