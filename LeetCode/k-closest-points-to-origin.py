857ffaa7c9a1b246ce6260058f59a9681e5a2385
Sanket Badhe <29823086+SankBad@users.noreply.github.com>
2021-01-10 04:06:31 -0500

    Added another method

--- LeetCode/k-closest-points-to-origin.py
+++ LeetCode/k-closest-points-to-origin.py
@@ -1,7 +1,7 @@
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        points = sorted(points, key = self.myfunc)
-        return points[:K]
-    
-    def myfunc(self, x):
-        return x[0]**2 + x[1]**2
+        my_dict = {}
+        for i in points:
+            my_dict[tuple(i)] = i[0]**2 + i[1]**2
+        my_arr = [a for a, b in sorted(my_dict.items(), key = lambda x: x[1])]
+        return my_arr[:K]
