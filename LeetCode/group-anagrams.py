2d78a0da628ce571a5b01eb5040c677730a0da66
Sanket Badhe <29823086+SankBad@users.noreply.github.com>
2021-01-11 02:50:11 -0500

    Update group-anagrams.py

--- LeetCode/group-anagrams.py
+++ LeetCode/group-anagrams.py
@@ -1,10 +1,16 @@
+
+from collections import defaultdict 
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mydict = {}
+        my_dict =  defaultdict(list)
         for i in strs:
-            if tuple(sorted(i)) not in mydict:
-                mydict[tuple(sorted(i))] = [i]
-            else:
-                mydict[tuple(sorted(i))].append(i)
-                
-        return list(mydict.values())
+            j = i
+            i = "". join(sorted(i))
+            my_dict[i].append(j)
+        
+        ans = []
+        for i in my_dict.values():
+            ans.append(i)
+        
+        return ans
